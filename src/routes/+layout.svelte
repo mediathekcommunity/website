3899bfe92ceea918236bbaa13c48d5e8fe79b8e4
Bitti <Bitti09@users.noreply.github.com>
2025-01-11 21:52:24 +0100

    style(layout): Clean up and format layout component for consistency
    Signed-off-by: Bitti <Bitti09@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,59 +1,45 @@
 <script>
-	import Header from '$lib/components/header.svelte';
-	import Footer from '$lib/components/footer.svelte';  // Renamed Footer1 to Footer
-	import '../app.css';
+    import Header from '$lib/components/header.svelte';
+    import Footer from '$lib/components/footer.svelte';
+    import '../app.css';
 </script>
 
 <div class="app">
-	<Header />
-	<main>
-		<slot />
-	</main>
-	<Footer />
+    <Header />
+    <main>
+        <slot />
+    </main>
+    <Footer />
 </div>
 
 <style>
-	:global(body) {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
-		background-color: #141414;
-		color: #fff;
-		font-family: Arial, sans-serif;
-	}
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        background-color: #141414;
+        color: #fff;
+        font-family: Arial, sans-serif;
+    }
 
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-	.b21 {
-		padding: 0 4% 0 4%;
-	}
-	main {
-		flex: 1;
-		width: 100%;
-		max-width: 100%;
-		margin: 0 auto;
-		padding: 0;
-		box-sizing: border-box;
-	}
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
 
-	.gradient-text {
-		background: radial-gradient(circle, var(--tertiary-500), var(--primary-500));
-		background-clip: text;
-		/*color: transparent;*/
-	}
+    main {
+        flex: 1;
+        width: 100%;
+        max-width: 100%;
+        margin: 0 auto;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-	.gradient-text-light {
-		background: radial-gradient(circle, var(--tertiary-100), red);
-		background-clip: text;
-		/*color: transparent;*/
-	}
-
-	@media (max-width: 768px) {
-		main {
-			padding: 0;
-		}
-	}
+    @media (max-width: 768px) {
+        main {
+            padding: 0;
+        }
+    }
 </style>
