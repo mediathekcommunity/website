7e60567b31a340b41e9013c29039dc58e452fd3b
Bitti <Bitti09@users.noreply.github.com>
2025-02-01 22:44:50 +0100

    refactor: remove DIRECTUS_APIURL usage from deployment and build processes

--- src/lib/directus.js
+++ src/lib/directus.js
@@ -1,10 +1,7 @@
 import { createDirectus, rest } from '@directus/sdk';
- 
-import { env } from '$env/dynamic/private';
-console.log(env);
- const { DIRECTUS_APIURL } = env;
+  
  function getDirectusInstance(fetch) {
-	const directus = createDirectus(DIRECTUS_APIURL).with(rest());
+	const directus = createDirectus("https://api.mediathek.community").with(rest());
 	return directus;
 }
 export default getDirectusInstance;
