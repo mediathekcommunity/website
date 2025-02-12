6b1dc8e8edd845d54d66cf8336f762a84ac8d46f
Bitti <Bitti09@users.noreply.github.com>
2025-02-12 18:29:50 +0100

    Update directus.js
    Signed-off-by: Bitti <Bitti09@users.noreply.github.com>

--- src/lib/directus.js
+++ src/lib/directus.js
@@ -1,12 +1,10 @@
 import { createDirectus, rest } from '@directus/sdk';
-
 import { env } from '$env/dynamic/private';
-/*
-const { DIRECTUS_APIURL } = env;
-*/
-const DIRECTUS_APIURL = 'https://api.mediathek.community';
-function getDirectusInstance(fetch) {
-	const directus = createDirectus(DIRECTUS_APIURL).with(rest());
-	return directus;
+
+const DIRECTUS_APIURL = env.DIRECTUS_APIURL || 'https://api.mediathek.community';
+
+function getDirectusInstance() {
+  return createDirectus(DIRECTUS_APIURL).with(rest());
 }
+
 export default getDirectusInstance;
