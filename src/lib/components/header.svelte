3b6d8fda12952e495f75c0a8d5c80c31b392f3cb
Bitti <Bitti09@users.noreply.github.com>
2025-02-22 02:23:51 +0100

    Bump version number to v0.9.1 in header component

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -17,7 +17,7 @@
 				<h1 class="site-title">
 					<span class="text-primary">Mediathek</span>
 					<span class="text-white">Community</span>
-					<span class="badge badge-primary text-white">v0.9.0</span>
+					<span class="badge badge-primary text-white">v0.9.1</span>
 				</h1>
 			</a>
 		</div>
