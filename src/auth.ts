7272385b6c5db72e9ebc9d01d1012dc242ea064c
Bitti <Bitti09@users.noreply.github.com>
2025-07-12 22:59:34 +0200

    fix: update console log to display full raw environment variables for Auth0

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import Auth0 from "@auth/sveltekit/providers/auth0"
   import * as rawEnv from '$env/dynamic/private';
 
-console.log("Auth0 clientId", rawEnv.AUTH0_CLIENT_ID)
+console.log("Auth0 clientId", rawEnv)
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
     Auth0({
--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,8 +1,10 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as rawEnv from '$env/static/private';
+import * as rawEnv2 from '$env/dynamic/private';
+
 import * as schema from './schema';
-console.log('Database URL:',  rawEnv);
+console.log('Database URL:',  rawEnv2);
  const client = createClient({
     url: rawEnv.DATABASE_URL,
     authToken: rawEnv.DATABASE_AUTH_TOKEN,
