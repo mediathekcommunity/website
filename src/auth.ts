333ddc024a05df8b427081572b3dc767af214d39
Bitti <Bitti09@users.noreply.github.com>
2025-07-12 22:58:08 +0200

    fix: update import to use dynamic environment variables for Auth0 configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Auth0 from "@auth/sveltekit/providers/auth0"
-  import * as rawEnv from '$env/static/private';
+  import * as rawEnv from '$env/dynamic/private';
 
 console.log("Auth0 clientId", rawEnv.AUTH0_CLIENT_ID)
 export const { handle, signIn, signOut } = SvelteKitAuth({
